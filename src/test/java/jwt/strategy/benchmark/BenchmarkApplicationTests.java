b799450755dae13a196b0c3f2d8d3e47ad217c53
문고은 <>
2025-09-18 10:38:51 +0900

    init

---
+++ src/test/java/jwt/strategy/benchmark/BenchmarkApplicationTests.java
@@ -0,0 +1,13 @@
+package jwt.strategy.benchmark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BenchmarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
